7c7b7df115fa87b338ad4e5ca8a7227f73405383
Muhammad-Aqeel-2226 <0302muhammadaqeel389@gmail.com>
2025-09-16 09:39:52 +0500

    Connected to Database and fixed 'ready' will be depricated in next major update warning and some small fixes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-require('dotenv').config();
-const { Client, IntentsBitField } = require('discord.js');
-const eventHandler = require('./handlers/eventHandler');
+require("dotenv").config();
+const { Client, IntentsBitField } = require("discord.js");
+const eventHandler = require("./handlers/eventHandler");
+const { default: mongoose } = require("mongoose");
 
 const client = new Client({
   intents: [
@@ -12,5 +13,14 @@ const client = new Client({
 });
 
 eventHandler(client);
+
+(async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("✅ Connected to MongoDB");
+  } catch (error) {
+    console.log(`❌ Error connecting to database: ${error}`);
+  }
+})();
 
 client.login(process.env.TOKEN);
